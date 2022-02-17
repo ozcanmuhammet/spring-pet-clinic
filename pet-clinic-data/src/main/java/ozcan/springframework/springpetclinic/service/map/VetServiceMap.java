0242bb0d91660b425f031a8f23df12c1f3823a72
Muhammet Ozcan <ozcanmuhammet@gmail.com>
2022-02-17 23:46:27 +0300

    implement map based services, closes #6

---
+++ pet-clinic-data/src/main/java/ozcan/springframework/springpetclinic/service/map/VetServiceMap.java
@@ -0,0 +1,35 @@
+package ozcan.springframework.springpetclinic.service.map;
+
+import ozcan.springframework.springpetclinic.model.Vet;
+import ozcan.springframework.springpetclinic.service.CrudService;
+
+import javax.sql.rowset.CachedRowSet;
+import java.util.Set;
+
+public class VetServiceMap extends AbstractMapService<Vet, Long> implements CrudService<Vet, Long> {
+    @Override
+    public Set<Vet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Vet findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Vet save(Vet object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Vet object) {
+        super.delete(object);
+    }
+
+}
