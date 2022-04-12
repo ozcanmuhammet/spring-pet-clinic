3dbee69293409256f7dad7cb8df5114a21d8ad73
Muhammet Ozcan <ozcanmuhammet@gmail.com>
2022-04-12 13:29:17 +0300

    added specialities map impl, closes #31

---
+++ pet-clinic-data/src/main/java/ozcan/springframework/springpetclinic/service/SpecialitiesService.java
@@ -0,0 +1,6 @@
+package ozcan.springframework.springpetclinic.service;
+
+import ozcan.springframework.springpetclinic.model.Speciality;
+
+public interface SpecialitiesService extends CrudService<Speciality, Long>{
+}
---
+++ pet-clinic-data/src/main/java/ozcan/springframework/springpetclinic/service/map/SpecialitiesMapService.java
@@ -0,0 +1,35 @@
+package ozcan.springframework.springpetclinic.service.map;
+
+import org.springframework.stereotype.Service;
+import ozcan.springframework.springpetclinic.model.Speciality;
+import ozcan.springframework.springpetclinic.service.SpecialitiesService;
+
+import java.util.Set;
+
+@Service
+public class SpecialitiesMapService extends AbstractMapService<Speciality, Long> implements SpecialitiesService {
+    @Override
+    public Set<Speciality> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Speciality speciality) {
+        super.delete(speciality);
+    }
+
+    @Override
+    public Speciality save(Speciality speciality) {
+        return super.save(speciality);
+    }
+
+    @Override
+    public Speciality findById(Long id) {
+        return super.findById(id);
+    }
+}
