19cfe989377b7a9a62f462c0cc3f39284ace8806
Muhammet Ozcan <ozcanmuhammet@gmail.com>
2022-04-12 12:44:03 +0300

    added petTypes to bootstrap, closes #32

--- pet-clinic-web/src/main/java/ozcan/springframework/springpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/ozcan/springframework/springpetclinic/bootstrap/DataLoader.java
@@ -4,26 +4,36 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 import ozcan.springframework.springpetclinic.model.Owner;
+import ozcan.springframework.springpetclinic.model.PetType;
 import ozcan.springframework.springpetclinic.model.Vet;
 import ozcan.springframework.springpetclinic.service.OwnerService;
+import ozcan.springframework.springpetclinic.service.PetTypeService;
 import ozcan.springframework.springpetclinic.service.VetService;
-import ozcan.springframework.springpetclinic.service.map.OwnerServiceMap;
-import ozcan.springframework.springpetclinic.service.map.VetServiceMap;
 
 @Component
 public class DataLoader implements CommandLineRunner {
 
     private final OwnerService ownerService;
     private final VetService vetService;
+    private final PetTypeService petTypeService;
 
     @Autowired  // This is not necessary, it's optional
-    public DataLoader(OwnerService ownerService, VetService vetService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petTypeService = petTypeService;
     }
 
     @Override
     public void run(String... args) throws Exception {
+
+        PetType dog = new PetType();
+        dog.setName("Dog");
+        PetType savedDogPetType = petTypeService.save(dog);
+
+        PetType cat = new PetType();
+        cat.setName("Cat");
+        PetType savedCatPetType = petTypeService.save(cat);
 
         Owner owner1 = new Owner();
         owner1.setFirstName("Michael");
