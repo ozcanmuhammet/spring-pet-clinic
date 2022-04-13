93dda60b9c90fe3a89998d4e18748c5a9e281fa1
Muhammet Ozcan <ozcanmuhammet@gmail.com>
2022-04-13 17:54:30 +0300

    added spring data jpa service for specialities, closes #44

---
+++ pet-clinic-data/src/main/java/ozcan/springframework/springpetclinic/service/springdatajpa/SpecialityJpaService.java
@@ -0,0 +1,48 @@
+package ozcan.springframework.springpetclinic.service.springdatajpa;
+
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+import ozcan.springframework.springpetclinic.model.Speciality;
+import ozcan.springframework.springpetclinic.repository.SpecialityRepository;
+import ozcan.springframework.springpetclinic.service.SpecialityService;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("springdatajpa")
+public class SpecialityJpaService implements SpecialityService {
+
+    private final SpecialityRepository specialityRepository;
+
+    public SpecialityJpaService(SpecialityRepository specialityRepository) {
+        this.specialityRepository = specialityRepository;
+    }
+
+    @Override
+    public Set<Speciality> findAll() {
+        Set<Speciality> specialities = new HashSet<>();
+        specialityRepository.findAll().forEach(specialities::add);
+        return specialities;
+    }
+
+    @Override
+    public Speciality findById(Long aLong) {
+        return specialityRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public Speciality save(Speciality object) {
+        return specialityRepository.save(object);
+    }
+
+    @Override
+    public void delete(Speciality object) {
+        specialityRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        specialityRepository.deleteById(aLong);
+    }
+}
